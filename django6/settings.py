20e93afe3a909e0511ca59191c74397be5611dc2
sachin810 <schnmthw@gmail.com>
2018-04-11 00:12:10 +0530

    Bug fix1

--- django6/settings.py
+++ django6/settings.py
@@ -123,3 +123,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")]
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn')
+
+MEDIA_URL = '/media/'
+MEDIAFILES_DIRS = [
+    os.path.join(BASE_DIR, "media")]
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media_cdn')
--- django6/urls.py
+++ django6/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('weathernow/', include('weathernow.urls')),
@@ -14,4 +14,5 @@ if settings.DEBUG:
 
 
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
